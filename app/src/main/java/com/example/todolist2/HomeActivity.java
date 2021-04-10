d16e7640ddd4b4e98ff78db6dcf9a7148033bd3d
muyij615 <63463317+muyij615@users.noreply.github.com>
2021-04-10 20:52:53 +0800

    first commit

---
+++ app/src/main/java/com/example/todolist2/HomeActivity.java
@@ -0,0 +1,332 @@
+package com.example.todolist2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
+
+    private DatabaseReference reference;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private String onlineUserID;
+
+    private ProgressDialog loader;
+
+    private String key = "";
+    private String task;
+    private String date;
+    private String description;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.HomeToolbar);
+        toolbar.getBackground().setAlpha(0);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        mAuth = FirebaseAuth.getInstance();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //set up a loader
+        loader = new ProgressDialog(this);
+
+        mUser = mAuth.getCurrentUser();
+        onlineUserID = mUser.getUid();
+        reference = FirebaseDatabase.getInstance().getReference().child("tasks").child(onlineUserID);
+
+
+        floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addTask();
+            }
+        });
+
+    }
+
+    //function that adds a task and upload to Firebase
+    private void addTask() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this); //create a alert dialog
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        //use the input_file layout as the view
+        View myView = inflater.inflate(R.layout.input_file, null);
+        myDialog.setView(myView);
+
+        final AlertDialog dialog = myDialog.create();
+        dialog.setCancelable(false); //touch outside doesn't cancel
+
+        //mke a round-corner dialog
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        //initialize the texts and btns
+        final EditText task = myView.findViewById(R.id.task);
+        final EditText description = myView.findViewById(R.id.description);
+        final EditText date = myView.findViewById(R.id.date);
+
+        Button save = myView.findViewById(R.id.saveBtn);
+        Button cancel = myView.findViewById(R.id.CancelBtn);
+
+        //if the user clicks cancel button
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        //if the user clicks save button
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mTask = task.getText().toString().trim();
+                String mDescription = description.getText().toString().trim();
+                String mdate = date.getText().toString().trim();
+                String id = reference.push().getKey();//get the key for each data set
+
+//                String date = DateFormat.getDateInstance().format(new Date());
+
+                if (TextUtils.isEmpty(mTask)) {
+                    task.setError("Task Required");
+                    return;
+                }
+                if (TextUtils.isEmpty(mDescription)) {
+                    description.setError("Description Required");
+                    return;
+                } else {
+                    loader.setMessage("Adding your task");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+
+                    //use the Model class to pack up the data
+                    Model model = new Model(mTask, mDescription, id, mdate);
+                    //update the data to Firebase
+                    reference.child(id).setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(HomeActivity.this, "Task has been inserted successfully", Toast.LENGTH_SHORT).show();
+                            } else {
+                                String error = task.getException().toString();
+                                Toast.makeText(HomeActivity.this, "Failed: " + error, Toast.LENGTH_SHORT).show();
+                            }
+                            loader.dismiss();
+                        }
+                    });
+
+                }
+
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();//show the dialog
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Model> options = new FirebaseRecyclerOptions.Builder<Model>().setQuery(reference,  Model.class).build();
+
+        FirebaseRecyclerAdapter<Model, MyViewHolder> adapter = new FirebaseRecyclerAdapter<Model, MyViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull MyViewHolder holder, int position, @NonNull Model model) {
+                holder.setDate(model.getDate());
+                holder.setTask(model.getTask());
+                holder.setDesc(model.getDescription());
+
+                holder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        key = getRef(position).getKey();
+                        task = model.getTask();
+                        description = model.getDescription();
+                        date = model.getDate();
+
+                        updateTask();
+                    }
+                });
+
+            }
+
+            @NonNull
+            @Override
+            public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.retrieved_layout, parent, false);
+                return new MyViewHolder(view);
+            }
+        };
+
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        View mView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setTask(String task){
+            TextView taskTextView = mView.findViewById(R.id.taskTv);
+            taskTextView.setText(task);
+        }
+
+        public void setDesc(String desc){
+            TextView descTextView = mView.findViewById(R.id.descriptionTv);
+            descTextView.setText(desc);
+        }
+
+        public void setDate(String date){
+            TextView dateTextView = mView.findViewById(R.id.dateTv);
+            dateTextView.setText(date);
+        }
+    }
+
+    private void updateTask(){
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.update_data, null);
+        myDialog.setView(view);
+
+        AlertDialog dialog = myDialog.create();
+
+        //make the corner round
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        EditText mTask = view.findViewById(R.id.mEditedTask);
+        EditText mDescription = view.findViewById(R.id.mEditedDescription);
+        EditText mDate = view.findViewById(R.id.mEditedDate);
+
+        mTask.setText(task);
+        mTask.setSelection(task.length());
+
+        mDescription.setText(description);
+        mDescription.setSelection(description.length());
+
+        mDate.setText(date);
+        mDate.setSelection(date.length());
+
+        Button deleteBtn = view.findViewById(R.id.btnDelete);
+        Button updateBtn = view.findViewById(R.id.btnUpdate);
+
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                task = mTask.getText().toString().trim();
+                description = mDescription.getText().toString().trim();
+                date = mDate.getText().toString().trim();
+
+//                String date = DateFormat.getDateInstance().format(new Date());
+
+                Model model = new Model(task, description, key, date);
+
+                reference.child(key).setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(HomeActivity.this, "Task has been updated successfully", Toast.LENGTH_SHORT).show();
+                        } else{
+                            String error = task.getException().toString();
+                            Toast.makeText(HomeActivity.this, "Update failed" + error, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                dialog.dismiss();
+            }
+        });
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reference.child(key).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(HomeActivity.this, "Task has been deleted successfully", Toast.LENGTH_SHORT).show();
+                        } else{
+                            String error = task.getException().toString();
+                            Toast.makeText(HomeActivity.this, "Delete failed" + error, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+    //set up the log out function
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                mAuth.signOut();
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/todolist2/LoginActivity.java
@@ -0,0 +1,125 @@
+package com.example.todolist2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private EditText loginEmail, loginPwd;
+    private Button loginBtn;
+    private TextView loginQn;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loader;
+
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+        toolbar = findViewById(R.id.loginToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        mAuth = FirebaseAuth.getInstance();
+        loader = new ProgressDialog(this);
+
+//        //if logged in already go to home
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (user != null){
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPwd = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginButton);
+        loginQn = findViewById(R.id.loginPageQuestion);
+
+        loginQn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = loginEmail.getText().toString().trim();
+                String password = loginPwd.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    loginEmail.setError("A valid email is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    loginPwd.setError("A valid password is required");
+                    return;
+                } else{
+                    loader.setMessage("Login in progress");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+
+                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else{
+                                String error = task.getException().toString();
+                                Toast.makeText(LoginActivity.this, "Login failed" + error, Toast.LENGTH_SHORT).show();
+                            }
+                            loader.dismiss();
+                        }
+                    });
+
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(authStateListener);
+    }
+}
